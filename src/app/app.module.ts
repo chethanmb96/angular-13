8cf2714cf6e3c63dc091c544bc61f7cc0a8f4d47
Chethan MB <Chethan.mb96@gmail.com>
2026-06-13 14:30:23 +0530

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { MatRadioModule } from '@angular/material/radio';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
+//
 
 @NgModule({
   declarations: [
